46a3979d81e2d7f0176585c5e33daf20da7f4163
Dika Afty <dikaafty@gmail.com>
2024-08-23 14:45:43 +0700

    Make Function playRound Simpler

--- main.js
+++ main.js
@@ -43,26 +43,32 @@ let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
 
-  if(humanChoice.toLowerCase() === "rock" && computerChoice === "Scissors") {
+  humanChoice = humanChoice.toLowerCase();
+  computerChoice = computerChoice.toLowerCase();
+
+  // Check for the winning conditions
+  if (humanChoice === "rock" && computerChoice === "scissors") {
     console.log("You win, Rock beats Scissors");
     humanScore++;
-  } else if(humanChoice.toLowerCase() === "paper" && computerChoice === "Rock" ) {
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
     console.log("You win, Paper beats Rock");
     humanScore++;
-  } else if(humanChoice.toLowerCase() === "scissors" && computerChoice === "Paper") {
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
     console.log("You win, Scissors beats Paper");
     humanScore++;
-  } else if(humanChoice.toLowerCase() === "rock" && computerChoice === "Paper") {
+  } else if (humanChoice === "rock" && computerChoice === "paper") {
     console.log("You lose, Paper beats Rock");
     computerScore++;
-  } else if(humanChoice.toLowerCase() === "paper" && computerChoice === "Scissors") {
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
     console.log("You lose, Scissors beats Paper");
     computerScore++;
-  } else if(humanChoice.toLowerCase() === "scissors" && computerChoice === "Rock") {
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
     console.log("You lose, Rock beats Scissors");
     computerScore++;
-  } else if(humanChoice.toLowerCase() === computerChoice.toLowerCase()) {
+  } else if (humanChoice === computerChoice) {
     console.log("Tie!");
+  } else {
+    console.log("Invalid input"); // Handle invalid inputs
   }
 }
 
